3ec50bc8edbeaba1c94dffba7098018e755f5288
Alexandr <mr.droidcaxa@mail.ru>
2023-05-20 19:24:12 +0700

    Split func on 2 files hpp and cpp

--- src/ht.cpp
+++ src/ht.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <time.h>
@@ -6,11 +7,20 @@
 int main()
 {
     initialize();
+    sf::RenderWindow window1;
+    window1.create(sf::VideoMode(200,1), "Exit");
 
-    while (window.isOpen())
+ while (window1.isOpen())
     {
         processEvents();
         render();
+
+        sf::Event event;
+        while (window1.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window1.close();
+        }
     }
 
     return 0;
