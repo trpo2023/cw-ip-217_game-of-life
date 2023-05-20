f92948b208ab01c3cb042779f2ff2b0481659b1e
w0mb <danil-kovalev-04@mail.ru@users.noreply.github.com>
2023-05-20 23:04:44 +0700

    finaly 1 test has compiled leeeeeeesgogogogogogogogog

--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,31 @@
-//tilibom
+
+
+// Включаем заголовочные файлы с определениями функций
+#include "../src/helper.hpp"
+#include "test.hpp"
+// Тест для функции wrapValue
+TEST(HelperFunctionsTest, WrapValue) {
+  // Проверяем, что функция wrapValue возвращает ожидаемые значения
+  EXPECT_EQ(wrapValue(-1, 10), 9);
+  EXPECT_EQ(wrapValue(5, 10), 5);
+  EXPECT_EQ(wrapValue(10, 10), 0);
+}
+
+// Напишите дополнительные тесты для остальных функций вашего приложения
+
+int mainTest(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  // Проверяем наличие аргумента командной строки "nographic" для запуска только тестов без открытия графического окна
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "nographic") {
+      // Запускаем тесты без открытия графического окна
+      return RUN_ALL_TESTS();
+    }
+  }
+
+  // Открытие графического окна
+  // ...
+
+  return 0;
+}
